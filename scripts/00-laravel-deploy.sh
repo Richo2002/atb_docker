4b360029336328d292fea1659b9d556857e9bceb
Richo2002 <richo2002.44@gmail.com>
2022-12-02 11:59:37 +0100

    Dockerfile update

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 
-echo "install php dependencies ..."
-composer install --no-interaction --optimize-autoloader --no-dev
-
-echo "change /var/www/html user/group"
-chown -Rf node:node /var/www/html
-
-echo "install js dependencies..."
-npm install
-
 echo "Symbolique link"
 php artisan storage:link
-
-echo "build assets..."
-npm run build
 
 echo "Caching config..."
 php artisan config:cache
@@ -26,9 +14,6 @@ php artisan view:cache
 
 echo "Running migrations..."
 php artisan migrate --force
-
-echo "change /var/www/html user/group"
-chown -Rf nginx:nginx /var/www/html
 
 
 
