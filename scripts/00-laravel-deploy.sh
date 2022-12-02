d49d5d9001178b4b25869ca0652f358942ee50cd
Richo2002 <richo2002.44@gmail.com>
2022-12-02 10:17:54 +0100

    Dockerfile update

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "permission in public folder"
-chmod -R 777 /var/www/html/public
+chmod -R 777 /var/www/html
 
 echo "install php dependencies ..."
 composer install --no-dev --working-dir=/var/www/html
